924cba029c9cee259a68305bc03b3eb406d71766
antin0de <129655447+antin0de@users.noreply.github.com>
2023-10-18 18:32:47 +0000

    Add support for CRUD operations on channel resource

--- handlers/channel.go
+++ handlers/channel.go
@@ -3,23 +3,85 @@ package handlers
 import (
 	"net/http"
 
+	"antin0.de/comm-relay/models"
 	"github.com/gin-gonic/gin"
 )
+
+type CreateChannelRequestBody struct {
+	Name string `json:"name"`
+}
 
 // POST /createChannel
 func (h *HandlerParams) CreateChannel() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var requestBody CreateChannelRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+			return
+		}
+		if requestBody.Name == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "You must provide a name",
+			})
+			return
+		}
+
+		channel := models.Channel{Name: requestBody.Name}
+		result := h.Db.Create(&channel)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Could not create channel",
+			})
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
+			"id": channel.ID,
 		})
 	}
+}
+
+type UpdateChannelRequestBody struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 // POST /updateChannel
 func (h *HandlerParams) UpdateChannel() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var requestBody UpdateChannelRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+			return
+		}
+		if requestBody.Name == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "You must provide a name",
+			})
+			return
+		}
+
+		result := h.Db.Model(&models.Channel{}).Where("id = ?", requestBody.ID).Update("name", requestBody.Name)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Could not update channel",
+			})
+		}
+
+		if result.RowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "Channel not found",
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
+			"id": requestBody.ID,
 		})
 	}
 }
@@ -27,17 +89,52 @@ func (h *HandlerParams) UpdateChannel() gin.HandlerFunc {
 // POST /listChannels
 func (h *HandlerParams) ListChannels() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
-		})
+		var channels []models.Channel
+		h.Db.Find(&channels)
+
+		var response []gin.H
+		for _, channel := range channels {
+			response = append(response, gin.H{
+				"id":        channel.ID,
+				"name":      channel.Name,
+				"createdAt": channel.CreatedAt,
+				"updatedAt": channel.UpdatedAt,
+			})
+		}
+		c.JSON(http.StatusOK, response)
 	}
+}
+
+type DeleteChannelRequestBody struct {
+	ID int `json:"id"`
 }
 
 // POST /deleteChannel
 func (h *HandlerParams) DeleteChannel() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var requestBody DeleteChannelRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+			return
+		}
+		// delete a channel model with ID
+		result := h.Db.Delete(&models.Channel{}, requestBody.ID)
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Could not delete channel",
+			})
+		}
+		if result.RowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "Channel not found",
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
+			"id": requestBody.ID,
 		})
 	}
 }

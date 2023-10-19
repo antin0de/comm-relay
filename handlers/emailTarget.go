c137303b9a47f9782c54f85b24aaa27eb1be80c6
antin0de <129655447+antin0de@users.noreply.github.com>
2023-10-19 00:41:24 +0000

    Add implementation for email target CRUd

--- handlers/emailTarget.go
+++ handlers/emailTarget.go
@@ -53,20 +53,81 @@ func (h *HandlerParams) CreateEmailTarget() gin.HandlerFunc {
 	}
 }
 
+type UpdateEmailTargetRequestBody struct {
+	ID           int    `json:"id"`
+	EmailAddress string `json:"emailAddress"`
+}
+
 // POST /updateEmailTarget
 func (h *HandlerParams) UpdateEmailTarget() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var requestBody UpdateEmailTargetRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+			return
+		}
+		if requestBody.EmailAddress == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "You must provide an email address",
+			})
+			return
+		}
+
+		result := h.Db.Model(&models.EmailTarget{}).
+			Where("id = ?", requestBody.ID).
+			Update("email_address", requestBody.EmailAddress)
+
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Could not update email target",
+			})
+		}
+
+		if result.RowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "Email target not found",
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
+			"id": requestBody.ID,
 		})
 	}
+}
+
+type DeleteEmailTargetRequestBody struct {
+	ID int `json:"id"`
 }
 
 // POST /deleteEmailTarget
 func (h *HandlerParams) DeleteEmailTarget() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var requestBody DeleteEmailTargetRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+			return
+		}
+
+		result := h.Db.Delete(&models.EmailTarget{}, requestBody.ID)
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Could not delete email target",
+			})
+		}
+		if result.RowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "Email target not found",
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
+			"id": requestBody.ID,
 		})
 	}
 }
